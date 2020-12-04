90ba0cc42396ef7366f4e0bc5e7de849a98b856e
alinashchukina <65511928+alinashchukina@users.noreply.github.com>
2020-12-04 12:54:04 +0300

    Add files via upload

---
+++ Python/HW6/Task-5/task_5_test.py
@@ -0,0 +1,63 @@
+import urllib.request
+import json
+import pytest
+from unittest.mock import MagicMock
+
+
+API_URL = 'http://worldclockapi.com/api/json/utc/now'
+
+YMD_SEP = '-'
+YMD_SEP_INDEX = 4
+YMD_YEAR_SLICE = slice(None, YMD_SEP_INDEX)
+
+DMY_SEP = '.'
+DMY_SEP_INDEX = 5
+DMY_YEAR_SLICE = slice(DMY_SEP_INDEX + 1, DMY_SEP_INDEX + 5)
+
+
+def what_is_year_now() -> int:
+    """
+    Получает текущее время из API-worldclock
+    и извлекает из поля 'currentDateTime' год
+    Предположим, что currentDateTime может быть в двух форматах:
+      * YYYY-MM-DD - 2019-03-01
+      * DD.MM.YYYY - 01.03.2019
+    """
+    with urllib.request.urlopen(API_URL) as resp:
+        resp_json = json.load(resp)
+
+    datetime_str = resp_json['currentDateTime']
+    if datetime_str[YMD_SEP_INDEX] == YMD_SEP:
+        year_str = datetime_str[YMD_YEAR_SLICE]
+    elif datetime_str[DMY_SEP_INDEX] == DMY_SEP:
+        year_str = datetime_str[DMY_YEAR_SLICE]
+    else:
+        raise ValueError('Invalid format')
+
+    return int(year_str)
+
+
+def test_date_line():
+    """
+    test checks format with "-"
+    """
+    json.load = MagicMock(return_value={"currentDateTime": "2020-12-04"})
+    assert 2020 == what_is_year_now()
+
+
+def test_date_point():
+    """
+    test checks format with "."
+    """
+    json.load = MagicMock(return_value={"currentDateTime": "04.12.2020"})
+    assert 2020 == what_is_year_now()
+
+
+def test_date_error():
+    """
+    test checks that any other separator causes ValueError
+    """
+    with pytest.raises(ValueError):
+        json.load = MagicMock(
+            return_value={"currentDateTime": "04/12/2020"})
+        what_is_year_now()
